bbcc6492a2ba0a6bfa416346ce975b1c2b066b93
kelvin-fernandes <sokelvinfernandes@gmail.com>
2019-08-19 21:36:28 -0400

    added index with new project method

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+})
+
+const projects = [];
+
+server.post('/projects', (req, res) => {
+    const project = req.body;
+    project.tasks = [];
+    projects.push(project);
+
+    return res.json(projects);
+})
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log(`server listening on ${PORT}`);
+});
